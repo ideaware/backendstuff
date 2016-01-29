704faddcc330a55df91bb1ab44679fdfa8ffe89b
Francisco Quintero <frajaquico@yahoo.es>
2016-01-29 13:52:01 -0500

    fix default version for ruby in rvm

--- provisioners/provision_macsa_server.sh
+++ provisioners/provision_macsa_server.sh
@@ -29,6 +29,9 @@ else
   echo "--> Installing ruby-2.2.2 and rvm ..."
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable
+  source ~/.rvm/scripts/rvm
+  rvm install 2.2.2
+  rvm use 2.2.2 --default
   su macsa -c "echo +ruby/2.2.2 >> /home/macsa/.provisioning-progress"
   echo "--> ruby-2.2.2 is now installed."
 fi
