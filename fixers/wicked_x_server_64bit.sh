c19a255645d07a3d676eb0909dd2d2dca2b0ce5b
Francisco Quintero Linux Mint Thinkpad <frajaquico@yahoo.es>
2016-03-30 21:57:25 -0500

    rename files according to the architecture version they really correspond

--- fixers/wicked_x_server_64bit.sh
+++ fixers/wicked_x_server_64bit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# source http://stackoverflow.com/a/34947598/1407371
+# source http://www.stormconsultancy.co.uk/blog/development/generating-pdfs-in-rails-with-pdfkit-and-deploying-to-a-server/
 
-apt-get install xfonts-75dpi
-wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-trusty-i386.deb
-dpkg -i wkhtmltox-0.12.2_linux-trusty-i386.deb
+# first, installing dependencies
+aptitude install openssl build-essential xorg libssl-dev
 
-echo "Testing"
-wkhtmltopdf http://www.google.com google.pdf
+# for 64bits OS
+wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.9.9-static-amd64.tar.bz2
+tar xvjf wkhtmltopdf-0.9.9-static-amd64.tar.bz2
+mv wkhtmltopdf-amd64 /usr/local/bin/wkhtmltopdf
+chmod +x /usr/local/bin/wkhtmltopdf
