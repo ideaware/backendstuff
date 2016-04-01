ce510af54566d7e635e966319966f1ff3cee2fd7
Francisco Quintero <frajaquico@yahoo.es>
2016-04-01 16:19:51 -0500

    updating wkhtmltopdf version for 64bit linux oses architecture

--- dependencies/install_wkhtmltopdf.sh
+++ dependencies/install_wkhtmltopdf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Not very useful
+
 add-apt-repository ppa:ecometrica/servers
 apt-get update
 apt-get install wkhtmltopdf
--- fixers/wicked_x_server_64bit.sh
+++ fixers/wicked_x_server_64bit.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# .deb process
+# first dependencies
+# source http://stackoverflow.com/a/34947598/1407371
+apt-get install openssl build-essential xorg libssl-dev xfonts-75dpi
+
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb
+dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb
+
+echo "Testing"
+wkhtmltopdf http://www.google.com google.pdf
+
+# old way not so reliable
 # source http://www.stormconsultancy.co.uk/blog/development/generating-pdfs-in-rails-with-pdfkit-and-deploying-to-a-server/
-
-# first, installing dependencies
-aptitude install openssl build-essential xorg libssl-dev
-
-# for 64bits OS
-wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.9.9-static-amd64.tar.bz2
-tar xvjf wkhtmltopdf-0.9.9-static-amd64.tar.bz2
-mv wkhtmltopdf-amd64 /usr/local/bin/wkhtmltopdf
-chmod +x /usr/local/bin/wkhtmltopdf
+# tar process
+#wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.9.9-static-amd64.tar.bz2
+#tar xvjf wkhtmltopdf-0.9.9-static-amd64.tar.bz2
+#mv wkhtmltopdf-amd64 /usr/local/bin/wkhtmltopdf
+#chmod +x /usr/local/bin/wkhtmltopdf
