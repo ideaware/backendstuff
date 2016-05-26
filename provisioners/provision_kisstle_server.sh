196635341abbe6152c95c7cf29e75ad13b391a2a
Francisco Quintero <frajaquico@yahoo.es>
2016-05-26 16:09:42 -0500

    remove ruby installation from provisioner due to previous required commands

--- provisioners/provision_kisstle_server.sh
+++ provisioners/provision_kisstle_server.sh
@@ -12,24 +12,14 @@ fi
 
 # Install rvm
 if grep -q +rvm .provisioning-progress; then
-  echo "--> ruby-2.2.2 and rvm are installed, moving on."
+  echo "--> rvm are installed, moving on."
 else
-  echo "--> Installing ruby-2.2.2 and rvm ..."
+  echo "--> Installing rvm ..."
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable
   su kisstle -c "echo +rvm >> /home/kisstle/.provisioning-progress"
 
-  echo "Adding user to rvm group"
-  sudo usermod -a -G rvm `whoami`
-
-  echo "Installing ruby-2.2.2."
-  rvm install ruby-2.2.2
-  
-  echo "Setting ruby-2.2.2. as default"
-  rvm --default use ruby-2.2.2
-
-  echo "Install bundler"
-  gem install bundler --no-rdoc --no-ri
+  echo "Follow steps in dependencies/install_rvm.sh to install Ruby 2.2.2"
   
   echo "--> rvm is now installed."
 fi
