0b8ea447333f1e9e2700efb9868cddecbb211ba2
Francisco Quintero <frajaquico@yahoo.es>
2016-09-27 13:56:43 -0500

    Update RVM installation

--- dependencies/install_rvm.sh
+++ dependencies/install_rvm.sh
@@ -5,7 +5,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 \curl -sSL https://get.rvm.io | bash -s stable
 
 echo "Sourcing RVM and reloading shell"
-. ~/.rvm/scripts/rvm
+. /home/$(whoami)/.rvm/scripts/rvm
 
 echo "RVM requirements"
 rvm requirements
@@ -19,5 +19,8 @@ rvm --default use ruby-2.2.2
 echo "Install bundler"
 gem install bundler --no-rdoc --no-ri
 
+# This wouldn't work because it is executed in a different
+# shell which gets invoked inside the script and not the
+# terminal window
 echo "...again"
-. ~/.rvm/scripts/rvm
+. /home/$(whoami)/.rvm/scripts/rvm
