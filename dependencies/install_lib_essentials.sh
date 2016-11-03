1a381421f7926beb5e22ea45260c98ad2824fff0
Francisco Quintero <frajaquico@yahoo.es>
2016-11-03 10:46:32 -0500

    Update with software-properties-common

--- dependencies/install_lib_essentials.sh
+++ dependencies/install_lib_essentials.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install build-essential curl git-core python-software-properties htop vim libfontconfig1 libgmp-dev
+apt-get -y install build-essential curl git-core python-software-properties htop vim libfontconfig1 libgmp-dev software-properties-common
 apt-get -y install zlib1g-dev libssl-dev libreadline6-dev libyaml-dev libncurses5-dev libxml2-dev libxslt-dev libsqlite3-dev
--- provisioners/provision_kisstle_server.sh
+++ provisioners/provision_kisstle_server.sh
@@ -16,7 +16,7 @@ if grep -q +core-libs .provisioning-progress; then
 else
   echo "--> Installing core libs (git, curl, etc)..."
   apt-get update
-  apt-get -y install build-essential curl git-core python-software-properties htop vim libfontconfig1 libgmp-dev
+  apt-get -y install build-essential curl git-core python-software-properties htop vim libfontconfig1 libgmp-dev software-properties-common
   apt-get -y install zlib1g-dev libssl-dev libreadline6-dev libyaml-dev libncurses5-dev libxml2-dev libxslt-dev libsqlite3-dev
   su kisstle -c "echo +core-libs >> /home/kisstle/.provisioning-progress"
   echo "--> Core libs (git, curl, etc) are now installed."
