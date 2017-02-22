241d5ea99a5ceb60b996395b772d5f372624cae0
Francisco Quintero <frajaquico@yahoo.es>
2017-02-22 10:06:24 -0500

    Add unzip and tree to lib essentials

--- dependencies/install_lib_essentials.sh
+++ dependencies/install_lib_essentials.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 apt-get -y install build-essential curl git-core python-software-properties htop vim libfontconfig1 libgmp-dev software-properties-common
-apt-get -y install zlib1g-dev libssl-dev libreadline6-dev libyaml-dev libncurses5-dev libxml2-dev libxslt-dev libsqlite3-dev
+apt-get -y install zlib1g-dev libssl-dev libreadline6-dev libyaml-dev libncurses5-dev libxml2-dev libxslt-dev libsqlite3-dev unzip tree
