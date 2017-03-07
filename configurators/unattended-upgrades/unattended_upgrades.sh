83ba1ce9543c1effa7904217ffd7c7e4f503d5ab
Francisco Quintero <frajaquico@yahoo.es>
2017-03-07 16:41:30 -0500

    Add folder for unattended-upgrades configuration in Ubuntu 14.04

---
+++ configurators/unattended-upgrades/unattended_upgrades.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install or verify unattended upgrades is installed
+apt-get update
+apt-get install -y unattended-upgrades
+
+# Select Yes to reconfigure to generate file /etc/apt/apt.conf.d/20auto-upgrades
+dpkg-reconfigure unattended-upgrades
+
+# Change /etc/apt/apt.conf.d/20auto-upgrades for ./20auto-upgrades
+# See 20auto-upgrades explanation here: https://gist.github.com/cesc1989/b4c685b6ca41f777949780c9729a3b70
+rm /etc/apt/apt.conf.d/20auto-upgrades
+cp ./20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
