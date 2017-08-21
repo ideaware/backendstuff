fc4ddfdc95922ab34acb61297cd8bde5d1af7289
Francisco Quintero <frajaquico@yahoo.es>
2017-08-20 19:01:43 -0500

    Add more ways to update locales

--- dependencies/install_locale.sh
+++ dependencies/install_locale.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# This no longer works, don't know why
+# Sometimes, this doesn't work
 #
 # echo "LC_ALL=\"en_US.UTF-8\"" >> /etc/default/locale
 # locale-gen en_US.UTF-8
 # update-locale LANG=en_US.UTF-8
 
 # See http://askubuntu.com/a/144448/167553
+
+# Manually fixing locales:
+# A) sudo dpkg-reconfigure locales
+# B) sudo apt-get --reinstall install language-pack-en
+# C) sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+
+# See https://askubuntu.com/a/144244/167553
 
 echo export LANGUAGE=en >> ~/.profile
 echo export LC_ALL=en_US.UTF-8 >> ~/.profile
