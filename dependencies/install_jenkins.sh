08ab166042392cfa6df88f39b9fa2a18b04157b5
Francisco Quintero <frajaquico@yahoo.es>
2018-03-23 11:25:46 -0500

    Update jenkins instructions

--- dependencies/install_jenkins.sh
+++ dependencies/install_jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get -y install jenkins
