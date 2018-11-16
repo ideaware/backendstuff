3950ef1a3b34dfb9735e453a993eda2b82b777ec
Francisco Quintero <frajaquico@yahoo.es>
2018-11-16 10:51:26 -0500

    Several minor updates :D

--- dependencies/install_lib_essentials.sh
+++ dependencies/install_lib_essentials.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install build-essential curl git-core python-software-properties htop vim libfontconfig1 libgmp-dev software-properties-common
-apt-get -y install zlib1g-dev libssl-dev libreadline6-dev libyaml-dev libncurses5-dev libxml2-dev libxslt-dev libsqlite3-dev unzip tree
+apt-get -y install \
+  build-essential \
+  curl \
+  git-core \
+  python-software-properties \
+  htop \
+  vim \
+  libfontconfig1 \
+  libgmp-dev \
+  software-properties-common \
+  zlib1g-dev \
+  libssl-dev \
+  libreadline6-dev \
+  libyaml-dev \
+  libncurses5-dev \
+  libxml2-dev \
+  libxslt-dev \
+  libsqlite3-dev \
+  unzip \
+  tree
