361036700fa0a8b1e36b68e400dc9db452573a76
Francisco Quintero <frajaquico@yahoo.es>
2018-11-29 22:03:28 -0500

    Update mysql installation

--- dependencies/install_mysql.sh
+++ dependencies/install_mysql.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install libmysqlclient-dev
-apt-get -y install mysql-server-5.5
+apt-get -y install libmysqlclient-dev mysql-server-5.5
 apt-get -f install mysql-server
