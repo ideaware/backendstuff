cc628b76c0f6e30fd40733d99a3527e4a6409888
Francisco Quintero <frajaquico@yahoo.es>
2019-10-30 14:02:54 -0500

    Specify better way to install imagemagick in macos

--- macos/install_imagemagick.sh
+++ macos/install_imagemagick.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# http://stackoverflow.com/questions/25460047/cant-install-imagemagick-with-brew-on-mac-os-x-mavericks
+# Resources:
+#   - http://stackoverflow.com/questions/25460047/cant-install-imagemagick-with-brew-on-mac-os-x-mavericks
+#   - https://glucode.com/blog/installing-rmagick-gem-with-imagemagick-and-homebrew-on-macos-10-14-mojave
 
 brew update
-brew install imagemagick
+brew install imagemagick@6
