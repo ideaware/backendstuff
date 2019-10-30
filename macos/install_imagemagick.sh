96d1ae96151dfb6f475d9659ce67a3a29a9ee768
Francisco Quintero <frajaquico@yahoo.es>
2019-10-30 13:57:10 -0500

    No flags when installing imagemagick with homebrew

--- macos/install_imagemagick.sh
+++ macos/install_imagemagick.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew update
-brew install imagemagick --disable-openmp --build-from-source
-
 # http://stackoverflow.com/questions/25460047/cant-install-imagemagick-with-brew-on-mac-os-x-mavericks
+
+brew update
+brew install imagemagick
