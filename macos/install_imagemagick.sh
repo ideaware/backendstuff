ce886f9f363d926ce576348a8f8ad673f1a8c923
Francisco Quintero <frajaquico@yahoo.es>
2019-10-31 14:31:23 -0500

    Add indications for terminal to find imagemagick

--- macos/install_imagemagick.sh
+++ macos/install_imagemagick.sh
@@ -6,3 +6,17 @@
 
 brew update
 brew install imagemagick@6
+
+# Verify installation with command
+# 
+#     $ convert -version
+# 
+# If the command is not found by the terminal do:
+# 
+#     $ brew link imagemagick@6 --force
+# 
+# Or find the path of the installation and add it to `PATH`
+# 
+#     /usr/local/opt/imagemagick@6/bin
+# 
+# See comments beginning with https://github.com/rmagick/rmagick/issues/256#issuecomment-273113469
