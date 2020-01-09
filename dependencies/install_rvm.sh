a239dcd72ba5ca7c1ece69625737b69059bd0129
Francisco Quintero <frajaquico@yahoo.es>
2020-01-09 15:30:24 -0500

    Update gpg command to install rvm in Linux distros

--- dependencies/install_rvm.sh
+++ dependencies/install_rvm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Do not run as sudo or there could be trouble setting up RVM
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 
 echo "Sourcing RVM and reloading shell"
